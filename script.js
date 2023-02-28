f76c857ad22d41a7b43e7675ecef97e2036d439b
slurpeesz <khtbee@gmail.com>
2023-02-28 15:37:21 -0800

    added html,css, and js files

---
+++ script.js
@@ -0,0 +1,27 @@
+var questions = [
+  {
+    title:'What is the correct way to write a variable?',
+    choices: [ 'var myvariable = (name)', 'var myVariable = {variable}', 'var myvariable = "name"','var myVariable = "name"'],
+    answer: 'var myVariable = "name"',
+  },
+  {
+    title:'Which symbol represents a strict equal?',
+    choices: ['==', '+', '===', '=']
+    answer: '===',
+  },
+  {
+    title: "Inside which HTML element do we put the Javascript?",
+    choices: ['<js>','<script>','<java>','<javascripting>']
+    answer: '<script>',
+  },
+  {
+    title:'What is the correct way to write a Javascript array?'
+    choices:['var colors = ["red", "green","yellow"]','var colors = (1:"red",2:"green", 3:"yellow")','var colors = {"red","blue","yellow}','var colors = "red","green","yellow"']
+    answer: 'var colors = ["red","green","yellow"]',
+  },
+  {
+    title:'Which event occurs when the user clicks on an HTML element?',
+    choices: ['onpress', 'onenter','onclick','onstart'],
+    answer:'onclick'
+  },
+];
